d3a5ef1fc36a32b897e124dcbe30942012da7895
herehavetheseflowers <herehavetheseflowers@gmail.com>
2022-12-03 14:03:53 +0400

    Project setup

---
+++ src/router/index.tsx
@@ -0,0 +1,26 @@
+import { Suspense, lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Loader } from '../components';
+import { withPrivateRoute } from './withPrivateRoute';
+import { RouterList } from './routerList';
+
+import HomePage from '../pages/Home';
+//import AboutPage from '../pages/About';
+//const ServerErrorPage = lazy(() => import(/* webpackChunkName: "servererror" */ '../pages/ServerError'));
+//const NotFoundPage = lazy(() => import(/* webpackChunkName: "notfound" */ '../pages/NotFound'));
+
+export function Router() {
+  return (
+    <Suspense fallback={<Loader />}>
+      <Routes>
+        <Route path={RouterList.HOME}>
+          <Route index element={<HomePage />} />
+          {/* <Route path={RouterList.ABOUT} element={<AboutPage />} />
+          <Route path={RouterList.SERVER_ERROR} element={<ServerErrorPage />} />
+          <Route path={RouterList.NOT_FOUND} element={<NotFoundPage />} />
+          */}
+        </Route>
+      </Routes>
+    </Suspense>
+  );
+}
