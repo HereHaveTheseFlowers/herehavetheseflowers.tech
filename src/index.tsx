095f06d054f3620cf48a560eda4aa3e338fb6809
herehavetheseflowers <herehavetheseflowers@gmail.com>
2022-12-03 16:31:34 +0400

    Adds loader

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,12 @@ import ReactDOM from "react-dom/client";
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
+window.addEventListener('load', () => {
+  let loader = document.querySelector('.loader');
+  if(loader) loader.parentElement.removeChild(loader)
+  loader = null;
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
