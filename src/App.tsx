5bb23f469985b4ddc504f04baaa372be045d5873
HereHaveTheseFlowers <herehavetheseflowers@gmail.com>
2025-01-02 19:05:47 +0300

    Removes old code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import { Router } from './router';
-//import { ReactP5Wrapper } from '@p5-wrapper/react';
-//import sketch from './components/Sketch';
 
 export default function App() {
-  //const isMobile: boolean = window.matchMedia('(max-device-width: 480px)').matches;
-  //{!isMobile && <ReactP5Wrapper sketch={sketch} className='p5wrapper' />}
   return (
     <main className='app'>
       <Router />
